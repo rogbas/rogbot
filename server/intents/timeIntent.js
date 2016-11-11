182c39177ebae21b8c2791630c9e8f010f626ab4
rogbas <rogbas@gmail.com>
2016-11-11 18:34:46 -0200

    Time intent and service registry implemented

---
+++ server/intents/timeIntent.js
@@ -0,0 +1,27 @@
+'use strict';
+const request = require('superagent');
+
+module.exports.process = function process(intentData, registry, cb) {
+
+    if(intentData.intent[0].value != 'time')
+        return cb(new Error(`Expected time intent, got ${intentData.intent[0].value}`));
+
+    if(!intentData.location)
+        return cb(new Error('Missing location in time intent'));
+
+
+    const location = intentData.location[0].value.replace(/,.?rogbot/i, '');
+    
+    const service = registry.get('time');
+    if(!service) return cb(false, 'No service available');
+
+    request.get(`http://${service.ip}:${service.port}/service/${location}`, (err, res) => {
+        if(err || res.statusCode != 200 || !res.body.result) {
+            console.log(err);
+            return cb(false, `I had a problem finding the time in ${location}`);
+        }
+
+        return cb(false, `In ${location} it is now ${res.body.result}`);
+
+    });
+}
